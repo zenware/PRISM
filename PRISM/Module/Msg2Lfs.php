26e98efcf2b4d8f507f1cd913656a961ff0bb118
Jay Looney <jay.m.looney@gmail.com>
2013-10-30 18:39:56 -0500

    whoops missed these

---
+++ PRISM/Module/Msg2Lfs.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Msg2Lfs.php
+ * 
+ * @category   Module
+ * @package    PRISM
+ * @subpackage Module\Msg2Lfs
+ * @author     zenware (Jay Looney) <jay.m.looney@gmail.com>
+ * @license    http://opensource.org/licenses/MIT MIT License
+ * @link       https://github.com/zenware/PRISM/blob/devel/Module/Msg2Lfs.php
+ */
+
+namespace PRISM\Module;
+
+/**
+ * Msg2Lfs()
+ * 
+ * @category   Module
+ * @package    PRISM
+ * @subpackage Module\Msg2Lfs
+ * @author     Dygear (Mark Tomlin) <Dygear@gmail.com>
+ * @author     ripnet (Tom Young) <ripnet@gmail.com>
+ * @author     morpha (Constantin Köpplinger) <morpha@xigmo.net>
+ * @author     Victor (Victor van Vlaardingen) <vic@lfs.net>
+ * @author     GeForz (Kai Lochbaum)
+ * @license    http://opensource.org/licenses/MIT MIT License
+ * @link       http://lfsforum.net/forumdisplay.php?f=312
+ */
+class Msg2Lfs
+{
+    //Shit
+} function Msg2Lfs() { new Msg2Lfs(); }
